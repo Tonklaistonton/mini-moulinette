589e31ceed5158deb17af39c9ceeec7c3e0ff13f
kpiwan <kpiwan@student.42bangkok.com>
2026-05-20 17:53:45 +0700

    feat: change flag C05/ex05 timedout

--- mini-moul/tests/C05/ex05/ft_sqrt.c
+++ mini-moul/tests/C05/ex05/ft_sqrt.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <fcntl.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <unistd.h>
 #include "../../../../ex05/ft_sqrt.c"
 #include "../../../utils/constants.h"
 
@@ -13,6 +16,7 @@ typedef struct s_test
 } t_test;
 
 int run_tests(t_test *tests, int count);
+void run_int_max_warning_test(void);
 
 int main(void)
 {
@@ -47,15 +51,12 @@ int main(void)
             .n = 2147395600,
             .expected = 46340,
         },
-        {
-            .desc = "Square root of INT_MAX",
-            .n = 2147483647,
-            .expected = 0,
-        },
     };
     int count = sizeof(tests) / sizeof(tests[0]);
 
-    return run_tests(tests, count);
+    int error = run_tests(tests, count);
+    run_int_max_warning_test();
+    return error;
 }
 
 int run_tests(t_test *tests, int count)
@@ -79,4 +80,49 @@ int run_tests(t_test *tests, int count)
     }
 
     return error;
+}
+
+void run_int_max_warning_test(void)
+{
+    pid_t   pid;
+    int     status;
+    int     waited;
+    int     ticks;
+
+    pid = fork();
+    if (pid == -1)
+    {
+        printf("  " GREY "[warning] Could not run INT_MAX overflow check\n" DEFAULT);
+        return ;
+    }
+    if (pid == 0)
+    {
+        if (ft_sqrt(2147483647) == 0)
+            _exit(0);
+        _exit(2);
+    }
+    ticks = 0;
+    waited = 0;
+    while (ticks < 20)
+    {
+        waited = waitpid(pid, &status, WNOHANG);
+        if (waited == pid)
+            break ;
+        usleep(50000);
+        ticks++;
+    }
+    if (waited != pid)
+    {
+        kill(pid, SIGKILL);
+        waitpid(pid, &status, 0);
+        printf("  " GREY "[warning] ft_sqrt(INT_MAX) timed out; likely i * i overflow near 46341\n" DEFAULT);
+    }
+    else if (!WIFEXITED(status) || WEXITSTATUS(status) != 0)
+    {
+        printf("  " GREY "[warning] ft_sqrt(INT_MAX) should return 0; check for i * i overflow\n" DEFAULT);
+    }
+    else
+    {
+        printf("  " GREEN CHECKMARK GREY " [warning] INT_MAX overflow check passed\n" DEFAULT);
+    }
 }
